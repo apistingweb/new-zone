73002f3f8d817466044d56edc801e4218593e463
apistingweb <143703659+apistingweb@users.noreply.github.com>
2024-05-03 14:38:25 +0300

    Create cdn.js

---
+++ advertisements/cdn.js
@@ -0,0 +1,7 @@
+  /*==================================
+~> Name Template     : ADS by STING WEB
+~> Version	         : V 1.0
+~> Last Updated      : 01 - 5 - 2024   : ->  02:00 AM  +2 GMT
+~> Developer By      : STING WEB TEAM  - Facebook Page   : ->  https://www.facebook.com/stingweb.eg
+~> Desgin URL        : sting-web.blogspot.com   : ->  https://www.sting-web.com
+=========== [ STING WEB ] ==========*/
