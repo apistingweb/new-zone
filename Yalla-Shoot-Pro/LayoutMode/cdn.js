06fa4d9a20627fa1484b834319abfe96c49f242a
apistingweb <143703659+apistingweb@users.noreply.github.com>
2024-05-03 14:37:34 +0300

    Delete Yalla-Shoot-Pro/LayoutMode directory

--- Yalla-Shoot-Pro/LayoutMode/cdn.js
+++
@@ -1,7 +0,0 @@
-/*==================================
-~> Name Template     : Yalla Shoot Pro Premium
-~> Version	         : V 5.0
-~> Last Updated      : 01 - 5 - 2024   : ->  02:00 AM  +2 GMT
-~> Developer By      : STING WEB TEAM  - Facebook Page   : ->  https://www.facebook.com/stingweb.eg
-~> Desgin URL        : sting-web.blogspot.com   : ->  https://www.sting-web.com
-=========== [ STING WEB ] ==========*/
